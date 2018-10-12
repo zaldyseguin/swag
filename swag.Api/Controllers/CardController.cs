af98c07bf3b7683813d75c155c36c0dd7fcaf90d
Zaldy Seguin <948360+zaldyseguin@users.noreply.github.com>
2018-10-13 00:48:33 +0800

    Add files via upload

---
+++ swag.Api/Controllers/CardController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using swag.Core.DTO;
+using swag.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace swag.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardController:Controller
+    {
+        private readonly ICardService _cardService;
+
+        public CardController(ICardService cardService)
+        {
+            _cardService = cardService;
+        }
+        [HttpGet("")]
+        public async Task<IActionResult> Get()
+        {
+            var card = await _cardService.GetAsync("411111201811121");
+
+            return Json(card);
+        }
+
+        [HttpGet("{cardnumber}")]
+        public async Task<IActionResult> Get(string cardnumber)
+        {
+            var response = new Response();
+
+            var card = await _cardService.GetAsync(cardnumber);
+            
+
+            if (card == null)
+                response=new Response { CardNumber = cardnumber, CardType = "Unknown", Result = "Does not Exist" };
+            else
+            {
+var factory = new CreditCardFactorycs() as ICreditCardName;
+                response = factory.GetCreditCardName(card).Result;
+            }
+
+            return Json(response);
+        }
+
+    }
+}
---
+++ swag.Api/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using swag.Core.EF;
+using swag.Core.Mappers;
+using swag.Core.Repositories;
+using swag.Core.Services;
+
+namespace swat.api
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
+            services.Configure<SqlSetting>(Configuration.GetSection("sql"));
+            // services.AddSingleton<IBucketRepository,InMemoryBucketRepository>();
+            services.AddScoped<ICardRepository, CardRepository>();
+            services.AddScoped<ICardService, CardService>();
+           // services.AddScoped<IDataInitializer, DataInitializer>();
+            services.AddSingleton<IMapper>(_ => AutoMapperConfig.GetMapper());
+            services.AddEntityFrameworkSqlServer()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .AddDbContext<SwagContext>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseHsts();
+            //}
+
+            app.UseHttpsRedirection();
+            //app.UseDefaultFiles();
+            app.UseMvc();
+        }
+    }
+}
